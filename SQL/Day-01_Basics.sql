4af88a17f4619daae49237293c150629abebcdf1
Shashank Singh <gwmsinghad@gmail.com>
2026-08-03 21:17:24 +0530

    Update SQL Day 01 - CREATE, INSERT, SELECT, UPDATE

--- SQL/Day-01_Basics.sql
+++ SQL/Day-01_Basics.sql
@@ -14,3 +14,26 @@ INSERT INTO Students
 VALUES (1, 'Shashank', 18, 'AIML');
 
 SELECT * FROM Students;
+
+
+
+-- Day 01 Continued
+
+CREATE TABLE Students (
+    StudentID NUMBER,
+    Name VARCHAR2(50),
+    Branch VARCHAR2(20)
+);
+
+DESC Students;
+
+INSERT INTO Students
+VALUES (1, 'Shashank', 'AIML');
+
+SELECT * FROM Students;
+
+UPDATE Students
+SET Branch = 'CSE-AIML'
+WHERE StudentID = 1;
+
+SELECT * FROM Students;
